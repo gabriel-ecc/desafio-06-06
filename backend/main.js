5bde56564d2eefbb8393d9fbc7996d3c5510412e
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-06-29 22:59:28 -0400

    configuraciones

--- backend/main.js
+++ backend/main.js
@@ -1,11 +1,21 @@
 import express from "express";
 import cors from "cors";
 import jwt from "jsonwebtoken";
-
+import bcript from "bcryptjs";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(3000,console.log("SERVER ON http://localhost:3000"));
+app.listen(3000, console.log("SERVER ON http://localhost:3000"));
+
+app.post("/usuarios", (req, res) => {
+  try {
+    const { email, password, rol, lenguage } = req.body;
+    const passwordEncrypt = bcript.hashSync(password, 10);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
